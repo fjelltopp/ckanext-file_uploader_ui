439a153a1bcb9fbdd3e6e4faaaf9cf9de6434374
Jonathan Berry <jonathan.s.berry@googlemail.com>
2020-07-28 14:25:57 +0100

    ADX-348 Setup travis tests.

--- bin/travis-run.sh
+++ bin/travis-run.sh
@@ -4,11 +4,19 @@ echo "NO_START=0\nJETTY_HOST=127.0.0.1\nJETTY_PORT=8983\nJAVA_HOME=$JAVA_HOME" |
 sudo cp ckan/ckan/config/solr/schema.xml /etc/solr/conf/schema.xml
 sudo service jetty restart
 
+flake8 --version
+# stop the build if there are Python syntax errors or undefined names
+flake8 . --count --select=E901,E999,F821,F822,F823 --show-source --statistics --exclude ckan
+
 nosetests --ckan \
           --nologcapture \
           --with-pylons=subdir/test.ini \
           --with-coverage \
-          --cover-package=ckanext.file_uploader_ui \
+          --cover-package=ckanext.restricted \
           --cover-inclusive \
           --cover-erase \
-          --cover-tests
+          --cover-tests \
+          ckanext/file_uploader_ui
+
+# strict linting
+flake8 . --count --max-complexity=10 --max-line-length=127 --statistics --exclude ckan
